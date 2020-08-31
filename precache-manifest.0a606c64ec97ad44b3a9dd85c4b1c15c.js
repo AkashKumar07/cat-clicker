6f5c58172b4417d516f770684f6426b2e6c431f6
shinchan <akashkumar3432@gmail.com>
2020-08-31 09:10:57 -0700

    Updates

---
+++ precache-manifest.0a606c64ec97ad44b3a9dd85c4b1c15c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f37f588919cd707a067ab689630f316",
+    "url": "/cat-clicker/index.html"
+  },
+  {
+    "revision": "7c702f0e78712e85a83b",
+    "url": "/cat-clicker/static/css/main.4510b2a4.chunk.css"
+  },
+  {
+    "revision": "e4470f842797f18a7602",
+    "url": "/cat-clicker/static/js/2.af90cb4e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cat-clicker/static/js/2.af90cb4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c702f0e78712e85a83b",
+    "url": "/cat-clicker/static/js/main.43fcb026.chunk.js"
+  },
+  {
+    "revision": "b8d6e2a4b9c50b506888",
+    "url": "/cat-clicker/static/js/runtime-main.73e78fe4.js"
+  }
+]);
